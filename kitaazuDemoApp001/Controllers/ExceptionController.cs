9be94c7b9e49e9c4f9f2a7fcedd1f7c021898fbb
YASUAKI MATSUDA <dark_crash@hotmail.com>
2016-02-28 14:12:07 +0900

    Exception

---
+++ kitaazuDemoApp001/Controllers/ExceptionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kitaazuDemoApp001.Controllers
+{
+    public class ExceptionController : Controller
+    {
+        // GET: Exception
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ThrowNull()
+        {
+            throw new NullReferenceException("そんなものはない");
+        }
+
+        public ActionResult ThrowIndexOutOfRange()
+        {
+            throw new IndexOutOfRangeException("そんなものはない");
+        }
+
+        public ActionResult ThrowNotImplements()
+        {
+            throw new NotImplementedException("そんなものはない");
+        }
+
+
+    }
+}
