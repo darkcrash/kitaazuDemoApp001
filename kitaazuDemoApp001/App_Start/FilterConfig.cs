5b9ac3259c9922093a2ad132302773be9a88d968
YASUAKI MATSUDA <dark_crash@hotmail.com>
2016-03-05 12:39:14 +0900

    add custom event application insights

--- kitaazuDemoApp001/App_Start/FilterConfig.cs
+++ kitaazuDemoApp001/App_Start/FilterConfig.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using System.Web;
 using System.Web.Mvc;
+using System.Collections.Generic;
 
 namespace kitaazuDemoApp001
 {
@@ -9,18 +11,50 @@ namespace kitaazuDemoApp001
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AppIdAttribute());
         }
     }
 
-    public class AppIdAttribute : ActionFilterAttribute {
+    public class AppIdAttribute : ActionFilterAttribute
+    {
 
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            base.OnActionExecuting(filterContext);
+
+            base.OnActionExecuted(filterContext);
 
             var appInsights = new TelemetryClient();
-            appInsights.Context.User.AccountId = filterContext.HttpContext.User.Identity.Name;
 
+            var inContext = appInsights.Context.GetInternalContext();
+
+
+            //appInsights.Context.User.AccountId = filterContext.HttpContext.User.Identity.Name;
+
+            appInsights.Context.Properties.Add("user", filterContext.HttpContext.User.Identity.Name);
+
+            appInsights.Context.User.Id = filterContext.HttpContext.User.Identity.Name;
+
+            var mailAddress = filterContext.HttpContext.User.Identity.Name;
+            var dic = new Dictionary<string, string>();
+            dic.Add("mail", mailAddress);
+
+            appInsights.TrackEvent("userEvent", dic);
+        }
+
+
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            var appInsights = new TelemetryClient();
+
+            base.OnResultExecuted(filterContext);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            var appInsights = new TelemetryClient();
+
+            base.OnResultExecuting(filterContext);
         }
 
     }
